7d2ddced58730d88fd39d837398465752d1176fd
Tushar Mittal <tushar.mittal@mountblue.tech>
2020-03-06 09:59:22 +0530

    Classes made for weather and football tasks

--- football_task.py
+++ football_task.py
@@ -1,22 +1,16 @@
-#import commontask
+from common_task import *
 
 class Football:
-    
+
     def __init__(self):
-        self.football_data=[line.strip().split() for line in open('football.dat').readlines()]
+        self.football_data = data.data_extract("football.dat")
         self.team_goal_range=["",100,0]
-    
-    
-    def smallest_goal_diff(self):
-        for data_row in self.football_data[1:]:
-            if len(data_row)!=1:
-                if abs((int(data_row[6])-int(data_row[8])))<abs((self.team_goal_range[1]-self.team_goal_range[2])):
-                    self.team_goal_range[0]=data_row[1]
-                    self.team_goal_range[1]=int(data_row[6])
-                    self.team_goal_range[2]=int(data_row[8])
+        self.team = ""
+        self.goal_diff = 0
 
-        print("Team is {}, Goals difference = {}".format(self.team_goal_range[0], abs(self.team_goal_range[1] - self.team_goal_range[2])))         
+    def goal_diff_return(self):
+        self.team, self.goal_diff = difference.smallest_goal_diff(self.football_data,self.team_goal_range)
+        print("Team is {}, Goals difference = {}".format(self.team, self.goal_diff))  
 
-        
-team_goal_diff=Football()
-team_goal_diff.smallest_goal_diff()
+calc_min_goal_diff = Football()
+calc_min_goal_diff.goal_diff_return()       
--- weather_task.py
+++ weather_task.py
@@ -1,26 +1,21 @@
+from common_task import *
+
 class Weather:
-    
+
     def __init__(self):
-        self.weather_data=[row.strip().split() for row in open('weather.dat').readlines()]
-        self.day_temp_range=[0,100,0]
-    
-    
-    def smallest_temp_spread(self):
-        for row in self.weather_data[2:]:
-            
-            if row[1][-1]=='*':
-                row[1]=row[1][:2]
-            
-            if row[2][-1]=='*':
-                row[2]=row[2][:2]
-            
-            if (float(row[1])-float(row[2]))<(self.day_temp_range[1]-self.day_temp_range[2]):
-                self.day_temp_range[0]=int(row[0])
-                self.day_temp_range[1]=float(row[1])
-                self.day_temp_range[2]=float(row[2])
+        self.weather_data = data.data_extract("weather.dat")
+        self.day_temp_range = [0,100,0]
+        self.day = 0
+        self.max_temp = 0
+        self.min_temp = 0
 
-        print("Day is {}, Max temp = {}, Min Temp = {}".format(self.day_temp_range[0], self.day_temp_range[1], self.day_temp_range[2]))            
+    def smallest_temperatue_spread(self):
+        self.day, self.max_temp, self.min_temp = difference.smallest_temp_spread(self.weather_data, self.day_temp_range)
+        print("Day is {}, Max temp = {}, Min Temp = {}".format(self.day, self.max_temp, self.min_temp))
 
-        
-day_temp_spread=Weather()
-day_temp_spread.smallest_temp_spread()        
+calc_weather_temp_spread = Weather()
+calc_weather_temp_spread.smallest_temperatue_spread()
+
+
+
+
